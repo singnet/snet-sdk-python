1401e33a3519a06c6b1acdd29d1d1f06e045c3ef
Deralden <121487413+Deralden@users.noreply.github.com>
2024-05-27 16:59:10 +0300

    Update run_all_functional.sh

--- testcases/utils/run_all_functional.sh
+++ testcases/utils/run_all_functional.sh
@@ -1,6 +1,6 @@
 #./packages/sdk/testcases/utils/reset_environment.sh
 
 cd testcases/functional_tests
-python test_sdk_client.py
+python3 test_sdk_client.py
 
-python test_prepaid_payment.py
+python3 test_prepaid_payment.py
