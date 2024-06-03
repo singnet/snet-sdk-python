7d6587936b0e936f24bb1d16ef17e75aff168144
Deralden <121487413+Deralden@users.noreply.github.com>
2024-06-03 17:38:38 +0300

    Update test_sdk_client.py

--- testcases/functional_tests/test_sdk_client.py
+++ testcases/functional_tests/test_sdk_client.py
@@ -12,10 +12,9 @@ class TestSDKClient(unittest.TestCase):
     # def test_verify_when_no_open_channel(self):
     #     channels = self.service_client.load_open_channels()
     #     self.assertEqual(0, len(channels))
-
-    def test_make_first_free_call(self):
-        result = self.service_client.call_rpc("mul", "Numbers", a=20, b=4)
-        self.assertEqual(80.0, result.value)
+   def test_make_first_free_call(self):
+      result = self.service_client.call_rpc("mul", "Numbers", a=20, b=4)
+      self.assertEqual(80.0, result.value)
 
     # def test_make_second_free_call(self):
     #     result = self.service_client.call_rpc("mul", "Numbers", a=20, b=5)
