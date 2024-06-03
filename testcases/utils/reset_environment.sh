b3eb21f0bc9428f841bb608849ff267c359a4cb4
Deralden <121487413+Deralden@users.noreply.github.com>
2024-06-03 18:19:15 +0300

    Update reset_environment.sh

--- testcases/utils/reset_environment.sh
+++ testcases/utils/reset_environment.sh
@@ -28,35 +28,35 @@ killall node || echo "supress an error"
 cd ../platform-contracts
 nohup ./node_modules/.bin/ganache-cli --mnemonic 'gauge enact biology destroy normal tunnel slight slide wide sauce ladder produce' --networkId 829257324 >/dev/null &
 ./node_modules/.bin/truffle migrate --network local
-=============================
-# # III. remove old snet-cli configuration
-# rm -rf ~/.snet
 
-# # IV. Configure SNET-CLI.
+# III. remove old snet-cli configuration
+rm -rf ~/.snet
 
-# # set correct ipfs endpoint
-# # (the new new configuration file with default values will be created automatically)
-# snet set default_ipfs_endpoint http://localhost:5002
+# IV. Configure SNET-CLI.
 
-# # Add local network and switch to it
-# snet network create local http://localhost:8545
+# set correct ipfs endpoint
+# (the new new configuration file with default values will be created automatically)
+snet set default_ipfs_endpoint http://localhost:5002
 
-# # swith to local network
-# snet network local
+# Add local network and switch to it
+snet network create local http://localhost:8545
 
-# # Configure contract addresses for local network (it will not be necessary for goerli or mainnet! )
-# snet set current_singularitynettoken_at 0x6e5f20669177f5bdf3703ec5ea9c4d4fe3aabd14
-# snet set current_registry_at 0x4e74fefa82e83e0964f0d9f53c68e03f7298a8b2
-# snet set current_multipartyescrow_at 0x5c7a4290f6f8ff64c69eeffdfafc8644a4ec3a4e
+# swith to local network
+snet network local
 
-# # Create First identity (snet-user = first ganache).
-# # (snet will automatically swith to this new identity)
-# snet identity create snet-user rpc --network local
-# export PYTHONPATH=$cwd
-# python $cwd"/packages/snet_cli/test/functional_tests/mint/mint.py"
-# snet account deposit 10000000 -y -q
-# snet account balance
-==============================
+# Configure contract addresses for local network (it will not be necessary for goerli or mainnet! )
+snet set current_singularitynettoken_at 0x6e5f20669177f5bdf3703ec5ea9c4d4fe3aabd14
+snet set current_registry_at 0x4e74fefa82e83e0964f0d9f53c68e03f7298a8b2
+snet set current_multipartyescrow_at 0x5c7a4290f6f8ff64c69eeffdfafc8644a4ec3a4e
+
+# Create First identity (snet-user = first ganache).
+# (snet will automatically swith to this new identity)
+snet identity create snet-user rpc --network local
+export PYTHONPATH=$cwd
+python $cwd"/packages/snet_cli/test/functional_tests/mint/mint.py"
+snet account deposit 10000000 -y -q
+snet account balance
+
 # service provider has --wallet-index==9 (0x52653A9091b5d5021bed06c5118D24b23620c529)
 # make two endpoints (both are actually valid)
 cd ../snet-sdk-python/testcases
