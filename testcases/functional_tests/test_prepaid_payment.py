3dc7e69ba6c58798951d29113699527c30c3de4d
Deralden <121487413+Deralden@users.noreply.github.com>
2024-06-07 16:09:46 +0300

    Update test_prepaid_payment.py

--- testcases/functional_tests/test_prepaid_payment.py
+++ testcases/functional_tests/test_prepaid_payment.py
@@ -27,21 +27,19 @@ def check_channel_status(service_client, last_signed_amount):
 
 
 def test_sdk():
-    org_id = "26072b8b6a0e448180f8c0e702ab6d2f"
-    service_id = "Exampleservice"
+    org_id = "test_org"
+    service_id = "test_service"
     group_name = "default_group"
 
     config = {
         "private_key": "0xc71478a6d0fe44e763649de0a0deb5a080b788eefbbcf9c6f7aef0dd5dbd67e0",
-        "eth_rpc_endpoint": "https://sepolia.infura.io/v3/09027f4a13e841d48dbfefc67e7685d5",
+        "eth_rpc_endpoint": "http://localhost:8545",
         "mpe_contract_address": "0x5c7a4290f6f8ff64c69eeffdfafc8644a4ec3a4e",
         "registry_contract_address": "0x4e74fefa82e83e0964f0d9f53c68e03f7298a8b2",
         "token_contract_address": "0x6e5f20669177f5bdf3703ec5ea9c4d4fe3aabd14",
-        "ipfs_rpc_endpoint": "/dns/ipfs.singularitynet.io/tcp/80/",
-        "org_id": org_id,
-        "service_id": service_id,
-        # "free_call_auth_token-bin": "f2548d27ffd319b9c05918eeac15ebab934e5cfcd68e1ec3db2b927653892959012b48da17a7973d57f72fac3c1eccd97862a4fa953c3726da65dec42f5989ee1b",
-        # "free-call-token-expiry-block": 172800,
+        "ipfs_rpc_endpoint": "http://localhost:5002",
+        "free_call_auth_token-bin": "f2548d27ffd319b9c05918eeac15ebab934e5cfcd68e1ec3db2b927653892959012b48da17a7973d57f72fac3c1eccd97862a4fa953c3726da65dec42f5989ee1b",
+        "free-call-token-expiry-block": 172800,
         "email": "test@test.com"
     }
 
