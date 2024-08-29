a9a8e317849a21eafcc1812425b73bda0109bee0
Dronny <andreypravdin52@gmail.com>
2024-08-29 14:01:36 +0300

    Finished console_app.py. Implemented updating channels.

--- examples/console_app.py
+++ examples/console_app.py
@@ -1,39 +1,28 @@
 from snet.sdk import SnetSDK
 from snet.sdk.service_client import ServiceClient
 
-
-def initialize_by_default():
-    org_id = "26072b8b6a0e448180f8c0e702ab6d2f"
-    service_id = "Exampleservice"
-    group_name = "default_group"
-    service = snet_sdk.create_service_client(org_id=org_id, service_id=service_id, group_name=group_name)
-    initialized_services.append(service)
-    global active_service
-    active_service = service
-
-
 def list_organizations():
     print("Organizations:")
     print(*map(lambda x: '\t' + x, snet_sdk.get_organization_list()), sep="\n")
 
 
 def list_services_for_org():
-    org_id = input("Enter organization id: ")
+    org_id = input("Enter organization id: ").strip()
     print("Services:")
     print(*map(lambda x: '\t' + x, snet_sdk.get_services_list(org_id=org_id)), sep="\n")
 
 
 def create_service_client():
-    org_id = input("Enter organization id: ")
-    service_id = input("Enter service id: ")
-    group_name = input("Enter payment group name: ")
+    org_id = input("Enter organization id: ").strip()
+    service_id = input("Enter service id: ").strip()
+    group_name = input("Enter payment group name: ").strip()
 
-    have_free_calls = input("Use free calls? (y/n): ") == 'y'
+    have_free_calls = input("Use free calls? (y/n): ").strip() == 'y'
     free_call_token = None
     free_call_token_expiry_block = None
     if have_free_calls:
-        free_call_token = input("Enter free call auth token: ")
-        free_call_token_expiry_block = input("Enter free call token expiry block: ")
+        free_call_token = input("Enter free call auth token: ").strip()
+        free_call_token_expiry_block = input("Enter free call token expiry block: ").strip()
 
     service = snet_sdk.create_service_client(org_id=org_id, service_id=service_id, group_name=group_name,
                                              free_call_auth_token_bin=free_call_token,
@@ -64,7 +53,7 @@ def list_initialized_services():
 
 def switch_service():
     list_initialized_services()
-    index = int(input("Enter the index of one of the initialized services: "))
+    index = int(input("Enter the index of one of the initialized services: ").strip())
     if index < len(initialized_services):
         global active_service
         active_service = initialized_services[index]
@@ -123,68 +112,115 @@ def deposit():
     snet_sdk.account.deposit_to_escrow_account(amount)
 
 
+def block_number():
+    if active_service is None:
+        print("No initialized services!\n"
+              "Please enter 'service' to go to the service menu and then enter 'add' to add a service.")
+        return None
+    print("Current block number: ", active_service.get_current_block_number())
+
+
+def update_channels():
+    if active_service is None:
+        print("No initialized services!\n"
+              "Please enter 'service' to go to the service menu and then enter 'add' to add a service.")
+        return None
+
+    is_continue = input("""Updating the channel list makes sense if the channel data has changed through other entry points. 
+This procedure may take several minutes. 
+Continue? (y/n): """).strip() == 'y'
+    if not is_continue:
+        return None
+
+    print("Updating the channel list...")
+    global channels
+    channels.clear()
+
+    for service in initialized_services:
+        load_channels = service.load_open_channels()
+        for channel in load_channels:
+            channels.append((channel, service.org_id, service.service_id, service.group['group_name']))
+
+    print("Channels updated! Enter 'list' to print the updated list.")
+
+
 def open_channel():
     global active_service
+    global channels
     additions = False
     if active_service is None:
-        additions = input("No initialized services! This entails entering additional parameters. Continue? (y/n): ") == 'y'
-        if not additions:
-            return None
+        print("No initialized services! The channel can only be opened for the service!\n"
+              "Please enter 'service' to go to the service menu and then enter 'add' to add a service.")
+        return None
     else:
-        is_continue = input("The new channel will be opened for the active service. Continue? (y/n): ") == 'y'
+        is_continue = input("The new channel will be opened for the active service. Continue? (y/n): ").strip() == 'y'
         if not is_continue:
             return None
 
-    amount = int(input("Enter amount of AGIX tokens in cogs to put into the channel: "))
+    amount = int(input("Enter amount of AGIX tokens in cogs to put into the channel: ").strip())
 
     balance = snet_sdk.account.escrow_balance()
     is_deposit = False
     if balance < amount:
         print(f"Insufficient balance!\n\tCurrent MPE balance: {balance}\n\tAmount to put: {amount}")
-        is_deposit = input("Would you like to deposit needed amount of AGIX tokens in advance? (y/n): ") == 'y'
+        is_deposit = input("Would you like to deposit needed amount of AGIX tokens in advance? (y/n): ").strip() == 'y'
         if not is_deposit:
             print("Channel is not opened!")
             return None
 
-    expiration = int(input("Enter expiration time in blocks: "))
+    expiration = int(input("Enter expiration time in blocks: ").strip())
 
-    if additions:
-        payment_address = input("Enter payment address: ")
-        group_id = input("Enter payment group id: ")
-        if is_deposit:
-            snet_sdk.mpe_contract.deposit_and_open_channel(account=snet_sdk.account,
-                                                           payment_address=payment_address,
-                                                           group_id=group_id,
-                                                           amount=amount,
-                                                           expiration=expiration)
-        else:
-            snet_sdk.mpe_contract.open_channel(account=snet_sdk.account,
-                                               payment_address=payment_address,
-                                               group_id=group_id,
-                                               amount=amount,
-                                               expiration=expiration)
+    if is_deposit:
+        channel = active_service.open_channel(amount=amount, expiration=expiration)
     else:
-        if is_deposit:
-            active_service.open_channel(amount=amount, expiration=expiration)
-        else:
-            active_service.deposit_and_open_channel(amount=amount, expiration=expiration)
+        channel = active_service.deposit_and_open_channel(amount=amount, expiration=expiration)
+    channels.append((channel, active_service.org_id, active_service.service_id, active_service.group['group_name']))
 
 
 def list_channels():
-    pass
+    global channels
+    print("ORGANIZATION_ID    SERVICE_ID    GROUP_NAME    CHANNEL_ID    AMOUNT    EXPIRATION")
+    for channel in channels:
+        channel[0].sync_state()
+        print(channel[1],
+              channel[2],
+              channel[3],
+              channel[0].channel_id,
+              channel[0].state['available_amount'],
+              channel[0].state['expiration'])
 
 
 def add_funds():
-    pass
+    channel_id = int(input("Enter channel id: ").strip())
+    exists = False
+    for channel in channels:
+        if channel[0].channel_id == channel_id:
+            amount = int(input("Enter amount of AGIX tokens in cogs to add to the channel: ").strip())
+            channel[0].add_funds(amount)
+            exists = True
+    if not exists:
+        print(f"Channel with id {channel_id} is not found!")
 
 
 def extend_expiration():
-    pass
+    channel_id = int(input("Enter channel id: ").strip())
+    exists = False
+    for channel in channels:
+        if channel[0].channel_id == channel_id:
+            expiration = int(input("Enter new expiration time in blocks: ").strip())
+            current_block = active_service.get_current_block_number()
+            if expiration < current_block:
+                print(f"Expiration time can't be less than current block ({current_block})!")
+                return None
+            channel[0].extend_expiration(expiration)
+            exists = True
+    if not exists:
+        print(f"Channel with id {channel_id} is not found!")
 
 
 config = {
     "private_key": 'APP_PROVIDER_PRIVATE_KEY',
-    "eth_rpc_endpoint": f"https://sepolia.infura.io/v3/APP_PROVIDER_INFURA_KEY",
+    "eth_rpc_endpoint": f"https://sepolia.infura.io/v3/APP_PROVIDER_INFURA_API_KEY",
     "concurrency": False,
     "identity_name": "NAME",
     "identity_type": "key",
@@ -195,8 +231,7 @@ config = {
 snet_sdk = SnetSDK(config)
 initialized_services = []
 active_service: ServiceClient
-channels = {}
-initialize_by_default()
+channels = []
 
 commands = {
     "main": {
@@ -204,6 +239,7 @@ commands = {
         "services": (list_services_for_org, "print a list of service ids for an organization from Registry"),
         # "change-config": change_config,
         "deposit": (deposit, "deposit AGIX tokens into MPE"),
+        "block": (block_number, "print the current block number"),
         "service": (lambda: None, "go to the services menu"),
         "channel": (lambda: None, "go to the channels menu"),
         "help": (commands_help, "print a list of available commands in the main menu"),
@@ -223,6 +259,7 @@ commands = {
     },
 
     "channel": {
+        "update": (update_channels, "update a list of initialized payment channels"),
         "list": (list_channels, "print a list of initialized payment channels"),
         "open": (open_channel, "open a new payment channel"),
         "add-funds": (add_funds, "add funds to a channel"),
@@ -247,7 +284,7 @@ To use the application, type the name of the command you want to execute.""")
 
     prefix = ">>> "
     while True:
-        command = input(prefix)
+        command = input(prefix).strip()
         if command in active_commands:
             active_commands[command][0]()
         else:
