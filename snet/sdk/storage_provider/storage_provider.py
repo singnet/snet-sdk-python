47957f23fc67fe81f1cbb5046445a03b74d67a83
Arondondon <andreypravdin52@gmail.com>
2024-10-08 18:15:34 +0300

    Fixed StorageProvider class and changed get_from_ipfs_and_checkhash() function.

--- snet/sdk/storage_provider/storage_provider.py
+++ snet/sdk/storage_provider/storage_provider.py
@@ -24,7 +24,7 @@ class StorageProvider(object):
         if org_provider_type == "ipfs":
             org_metadata_json = get_from_ipfs_and_checkhash(self._ipfs_client, org_metadata_hash)
         else:
-            _, org_metadata_json = self.lighthouse_client.download(org_metadata_uri)
+            org_metadata_json, _ = self.lighthouse_client.download(org_metadata_uri)
         org_metadata = json.loads(org_metadata_json)
 
         return org_metadata
@@ -43,7 +43,7 @@ class StorageProvider(object):
         if service_provider_type == "ipfs":
             service_metadata_json = get_from_ipfs_and_checkhash(self._ipfs_client, service_metadata_hash)
         else:
-            _, service_metadata_json = self.lighthouse_client.download(service_metadata_uri)
+            service_metadata_json, _ = self.lighthouse_client.download(service_metadata_uri)
         service_metadata = mpe_service_metadata_from_json(service_metadata_json)
 
         return service_metadata
--- snet/sdk/utils/ipfs_utils.py
+++ snet/sdk/utils/ipfs_utils.py
@@ -6,34 +6,26 @@ import multihash
 
 def get_from_ipfs_and_checkhash(ipfs_client, ipfs_hash_base58, validate=True):
     """
-    Get file from ipfs
-    We must check the hash becasue we cannot believe that ipfs_client wasn't been compromise
+    Get file from IPFS. If validate is True, verify the integrity of the file using its hash.
     """
+
+    data = ipfs_client.cat(ipfs_hash_base58)
+
     if validate:
-        from snet.sdk.resources.proto.unixfs_pb2 import Data
-        from snet.sdk.resources.proto.merckledag_pb2 import MerkleNode
-
-        # No nice Python library to parse ipfs blocks, so do it ourselves.
         block_data = ipfs_client.block.get(ipfs_hash_base58)
-        mn = MerkleNode()
-        mn.ParseFromString(block_data)
-        unixfs_data = Data()
-        unixfs_data.ParseFromString(mn.Data)
-        assert unixfs_data.Type == unixfs_data.DataType.Value(
-            'File'), "IPFS hash must be a file"
-        data = unixfs_data.Data
 
-        # multihash has a badly registered base58 codec, overwrite it...
-        multihash.CodecReg.register(
-            'base58', base58.b58encode, base58.b58decode)
-        # create a multihash object from our ipfs hash
-        mh = multihash.decode(ipfs_hash_base58.encode('ascii'), 'base58')
+        # print(f"IPFS hash (Base58): {ipfs_hash_base58}")
+        # print(f"Block data length: {len(block_data)}")
 
-        # Convenience method lets us directly use a multihash to verify data
-        if not mh.verify(block_data):
+        # Decode Base58 bash to multihash
+        try:
+            mh = multihash.decode(ipfs_hash_base58.encode('ascii'), "base58")
+        except Exception as e:
+            raise ValueError(f"Invalid multihash for IPFS hash: {ipfs_hash_base58}. Error: {str(e)}") from e
+
+        if not mh.verify(block_data):  # Correctly using mh instance for verification
             raise Exception("IPFS hash mismatch with data")
-    else:
-        data = ipfs_client.cat(ipfs_hash_base58)
+
     return data
 
 def get_ipfs_client(config):
