92475b3c1442a4da4c85d1960bc64d8a6ed81115
Arondondon <andreypravdin52@gmail.com>
2024-11-18 13:35:47 +0300

    Fixed a bug where stubs for 'training' or 'pricing' were imported instead of stubs for service

--- snet/sdk/__init__.py
+++ snet/sdk/__init__.py
@@ -95,8 +95,8 @@ class SnetSDK:
             lib_generator.generate_client_library()
         else:
             path_to_pb_files = self.get_path_to_pb_files(org_id, service_id)
-            pb_2_file_name = find_file_by_keyword(path_to_pb_files, keyword="pb2.py")
-            pb_2_grpc_file_name = find_file_by_keyword(path_to_pb_files, keyword="pb2_grpc.py")
+            pb_2_file_name = find_file_by_keyword(path_to_pb_files, keyword="pb2.py", exclude=['training', 'pricing'])
+            pb_2_grpc_file_name = find_file_by_keyword(path_to_pb_files, keyword="pb2_grpc.py", exclude=['training', 'pricing'])
             if not pb_2_file_name or not pb_2_grpc_file_name:
                 lib_generator.generate_client_library()
         
@@ -145,8 +145,10 @@ class SnetSDK:
     
     def get_module_by_keyword(self, org_id: str, service_id: str, keyword: str) -> ModuleName:
         path_to_pb_files = self.get_path_to_pb_files(org_id, service_id)
-        file_name = find_file_by_keyword(path_to_pb_files, keyword)
+        file_name = find_file_by_keyword(path_to_pb_files, keyword, exclude=['training', 'pricing'])
+        # print(f"File name: {file_name}")
         module_name = os.path.splitext(file_name)[0]
+        # print(f"Module name: {module_name}")
         return ModuleName(module_name)
 
     def get_service_metadata(self, org_id, service_id):
