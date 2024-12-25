888a26fd1d6d5e5f65cefa98934fe64826eaacbb
kiruxaspb <dvoreckovkirill@gmail.com>
2024-12-25 13:26:15 +0300

    Fixed package name for implement pep 625

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages, setup
 
 from version import __version__
 
-PACKAGE_NAME = 'snet.sdk'
+PACKAGE_NAME = 'snet-sdk'
 
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
@@ -19,7 +19,7 @@ requirements = requirements_str.split("\n")
 setup(
     name=PACKAGE_NAME,
     version=__version__,
-    packages=find_namespace_packages(include=['snet.*']),
+    packages=find_namespace_packages(include=['snet*']),
     namespace_packages=['snet'],
     url='https://github.com/singnet/snet-sdk-python',
     author='SingularityNET Foundation',
